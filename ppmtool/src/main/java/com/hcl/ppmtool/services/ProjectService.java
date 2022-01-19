da8d678190003437d2b40a2c7543e30e45b3d89a
Zachary Berghoff <zachary.berghoff@hcl.com>
2022-01-19 16:43:15 -0600

    added web and service packages

---
+++ ppmtool/src/main/java/com/hcl/ppmtool/services/ProjectService.java
@@ -0,0 +1,22 @@
+package com.hcl.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.ppmtool.domain.Project;
+import com.hcl.ppmtool.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	
+	public Project saveOrUpdateProject(Project project) {
+		
+		//Logic
+		
+		return projectRepository.save(project);
+	}
+}
