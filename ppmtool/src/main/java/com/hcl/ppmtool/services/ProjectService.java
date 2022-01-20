00d616a7cf92e6492e2d71952a7f8ecd65127ed6
Zachary Berghoff <zachary.berghoff@hcl.com>
2022-01-20 17:37:56 -0600

    delete an existing project

--- ppmtool/src/main/java/com/hcl/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/hcl/ppmtool/services/ProjectService.java
@@ -40,4 +40,14 @@ public class ProjectService {
 		return projectRepository.findAll();
 	}
 	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if(project == null) {
+			throw new ProjectIdException("Cannot delete project with ID '" + projectId + "'. This project does not exist.");
+		}
+		
+		projectRepository.delete(project);
+	}
+	
 }
