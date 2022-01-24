863c58f15e9992703cd3b82d60a13249280de3f0
Zachary Berghoff <zachary.berghoff@hcl.com>
2022-01-24 16:09:19 -0600

    react router, first functional component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,21 +1,24 @@
+import "./App.css";
 
-import './App.css';
-
-import React, { Component } from 'react';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React, { Component } from "react";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Header />  
-      <Dashboard />
-      </div>
-      );
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    );
   }
 }
-
 
 export default App;
